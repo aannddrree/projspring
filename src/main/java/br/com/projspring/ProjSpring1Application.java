69c6879f6330f1408b255b8c63aaf702bd4a29dc
Professor Andre Silva <andre@fiar.local>
2019-05-17 19:23:35 -0300

    Implantação

---
+++ src/main/java/br/com/projspring/ProjSpring1Application.java
@@ -0,0 +1,12 @@
+package br.com.projspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjSpring1Application.class, args);
+	}
+}
